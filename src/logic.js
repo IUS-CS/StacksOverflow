12b1e754ce3e25fbd93ebf76980e04327750cfc3
Sam Venter <sventer444@gmail.com>
2019-03-10 16:05:52 -0400

    refactored code to be more modular

--- src/logic.js
+++ src/logic.js
@@ -43,54 +43,25 @@
     
     var max = 5;
     
-    for (var i = 0; i < max; i++)
+    for (var i = 1; i <= max; i++)
     {
     var line = {x1:0, y1:0, x2:0, y2:0};
-    line.x1 = drawnLines[drawnLines.length-1].x2;
-    line.y1 = drawnLines[drawnLines.length-1].y2;
-    line.x2 = Math.floor(Math.random() *700);
-    line.y2 = Math.floor(Math.random() *700);
+    line.x1 = drawnLines[i-1].x2;
+    line.y1 = drawnLines[i-1].y2;
     drawnLines.push(line);
-    drawValidLine(drawnLines);
+    
+    do{
+    drawnLines[i].x2 = Math.floor(Math.random() *700);
+    drawnLines[i].y2 = Math.floor(Math.random() *700); 
     }
+    while(checkValidLine(drawnLines) == false);
+    path.lineTo(drawnLines[i].x2, drawnLines[i].y2);
+    }//for
+    
     //set end point
     var endLine = {x1: drawnLines[max].x2, y1: drawnLines[max].y2, x2:0, y2:0};
-    if(endLine.x1 < 400 && endLine.y1 <400){ //quadrant one
-      if(endLine.x1 < endLine.y1){
-      endLine.y2 = endLine.y1;
-      }
-      else{
-      endLine.x2 = endLine.x1;
-      }
-    }
-    else if (endLine.x1 >= 400 && endLine.y1 <400){ //quadrant two
-      if(800-endLine.x1 < endLine.y1){
-      endLine.y2 = endLine.y1;
-      endLine.x2 = 800;
-      }
-      else{
-      endLine.x2 = endLine.x1;
-      }
-    }//q2
-    else if (endLine.x1 < 400 && endLine.y1 >= 400){ //quadrant three
-    if(endLine.x1 < 800-endLine.y1){
-    endLine.y2 = endLine.y1;
-    }
-    else{
-    endLine.x2 = endLine.x1;
-    endLine.y2 =800;
-    }
-    }//q3
-    else{ //quadrant four
-    if(endLine.x1 > endLine.y1){
-    endLine.y2 = endLine.y1;
-    endLine.x2 =800;
-    }
-    else{
-    endLine.x2 = endLine.x1;
-    endLine.y2 =800;
-    }
-    }//q4
+    selectEndPoint(endLine);
+    
     drawnLines.push(endLine);
     path.lineTo(endLine.x2, endLine.y2);
     enemies = this.add.group();
@@ -112,34 +83,69 @@
     }
     }//create
     
-    function drawValidLine(drawnLines){
-    var j = drawnLines.length-2;
-    var valid = true;
-    var denominator;
-    var lineToDraw = drawnLines.length -1;
-    var t1;
-    var t2;
-    
-    while (valid && j >= 0){
-    //x1 y1, x2 y2 are line already drawn
-    //x3 y3, x4 y4 are line to be drawn
-    
-    denominator = ((drawnLines[lineToDraw].x2 - drawnLines[lineToDraw].x1)*(drawnLines[j].y1 - drawnLines[j].y2))-((drawnLines[j].x1 - drawnLines[j].x2)*(drawnLines[lineToDraw].y2 - drawnLines[lineToDraw].y1));
-    t1 = (((drawnLines[lineToDraw].y1-drawnLines[lineToDraw].y2)*(drawnLines[j].x1-drawnLines[lineToDraw].x1))+((drawnLines[lineToDraw].x2-drawnLines[lineToDraw].x1)*(drawnLines[j].y1-drawnLines[lineToDraw].y1)))/denominator;
-    t2 = (((drawnLines[j].y1-drawnLines[j].y2)*(drawnLines[j].x1-drawnLines[lineToDraw].x1))+((drawnLines[j].x2-drawnLines[j].x1)*(drawnLines[j].y1-drawnLines[lineToDraw].y1)))/denominator;
-    
-    if (0<t1 && t1<1 && 0<t2 && t2<1){
-    valid =false;
-    }
-    j--;
-    }//while
-    if(valid == false){
-    drawnLines[lineToDraw].x2 = Math.floor(Math.random() *700);
-    drawnLines[lineToDraw].y2 = Math.floor(Math.random() *700);
-    drawValidLine(drawnLines);
-    }//if
-    else path.lineTo(drawnLines[lineToDraw].x2, drawnLines[lineToDraw].y2);
+    function checkValidLine(drawnLines){
+        var j = drawnLines.length-2;
+        var valid = true;
+        var denominator;
+        var lineToDraw = drawnLines.length -1;
+        var t1;
+        var t2;
+        
+        while (valid && j >= 0){
+        //x1 y1, x2 y2 are line already drawn
+        //x3 y3, x4 y4 are line to be drawn
+        
+        denominator = ((drawnLines[lineToDraw].x2 - drawnLines[lineToDraw].x1)*(drawnLines[j].y1 - drawnLines[j].y2))-((drawnLines[j].x1 - drawnLines[j].x2)*(drawnLines[lineToDraw].y2 - drawnLines[lineToDraw].y1));
+        t1 = (((drawnLines[lineToDraw].y1-drawnLines[lineToDraw].y2)*(drawnLines[j].x1-drawnLines[lineToDraw].x1))+((drawnLines[lineToDraw].x2-drawnLines[lineToDraw].x1)*(drawnLines[j].y1-drawnLines[lineToDraw].y1)))/denominator;
+        t2 = (((drawnLines[j].y1-drawnLines[j].y2)*(drawnLines[j].x1-drawnLines[lineToDraw].x1))+((drawnLines[j].x2-drawnLines[j].x1)*(drawnLines[j].y1-drawnLines[lineToDraw].y1)))/denominator;
+        
+        if (0<t1 && t1<1 && 0<t2 && t2<1){
+        valid =false;
+        }
+        j--;
+        }//while
+        
+        return valid;
     }//draw valid line
+    
+    function selectEndPoint(endLine){
+            if(endLine.x1 < 400 && endLine.y1 <400){ //quadrant one
+          if(endLine.x1 < endLine.y1){
+          endLine.y2 = endLine.y1;
+          }
+          else{
+          endLine.x2 = endLine.x1;
+          }
+        }
+        else if (endLine.x1 >= 400 && endLine.y1 <400){ //quadrant two
+          if(800-endLine.x1 < endLine.y1){
+          endLine.y2 = endLine.y1;
+          endLine.x2 = 800;
+          }
+          else{
+          endLine.x2 = endLine.x1;
+          }
+        }//q2
+        else if (endLine.x1 < 400 && endLine.y1 >= 400){ //quadrant three
+        if(endLine.x1 < 800-endLine.y1){
+        endLine.y2 = endLine.y1;
+        }
+        else{
+        endLine.x2 = endLine.x1;
+        endLine.y2 =800;
+        }
+        }//q3
+        else{ //quadrant four
+        if(endLine.x1 > endLine.y1){
+        endLine.y2 = endLine.y1;
+        endLine.x2 =800;
+        }
+        else{
+        endLine.x2 = endLine.x1;
+        endLine.y2 =800;
+        }
+        }//q4
+    }//draw the ending point
 
     function update ()
     {
